75fbb0e71164620d035c262b955618f7233457fb
Pedro Castro <pedrohenriquealvesc94@gmail.com>
2022-09-23 15:19:49 -0300

    Update messageDelete.js

--- Events/Message/messageDelete.js
+++ Events/Message/messageDelete.js
@@ -17,7 +17,7 @@ module.exports = {
             Log.addField(`attachments`, `${message.attachments.map(a => a.url)}`, true)
         }
 
-        new WebhookClient({url: "https://discord.com/api/webhooks/950377293308645476/9Y977JDaVoseW2gOYeQap9OM-LjGPPqF9AxNG3eZ_PryGTStgdaVlRUQj3VXU1hi99yC"}
+        new WebhookClient({url: "your url"}
         ).send({embeds: [Log]}).catch((err) => { console.log(err)});
 
     }
